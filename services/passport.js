720197d8acb134319e827a5610a1a375aa050033
Ben <shilinchen1010@gmail.com>
2020-06-30 18:30:40 -0700

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -21,6 +21,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true
     },
     (acessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
