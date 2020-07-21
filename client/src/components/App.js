a68f05efc787e40bc9ddd0938c067031264a9973
Ben <shilinchen1010@gmail.com>
2020-07-21 14:19:29 -0700

    Added billing and client side app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 import Header from "./Header";
-import Landing from './Landing';
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
-
+import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
 
 class App extends Component {
-    componentDidMount() {
-        this.props.fetchUser();
-    }
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
   render() {
     return (
       <div>
         <BrowserRouter>
-          <div>
+          <div className="container">
             <Header />
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
@@ -30,6 +29,4 @@ class App extends Component {
   }
 }
 
-export default connect(null, actions)(App);  
-
-
+export default connect(null, actions)(App);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 const { request } = require("express");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI, {
@@ -29,12 +30,13 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoute")(app);
 
 if (process.env.NODE_ENV === "production") {
   // Express will server up production assets
   // like our main.js file, or main.css file!
   app.use(express.static("client/build"));
-  
+
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require("path");
